f715d6695fea29eba37dc2d52ea72f56485f919e
unknown <soumyagh@Insoumyagh.corporate.thoughtworks.com>
2013-12-30 12:02:43 +0530

    HashMap Started

--- hashMap/hashMap.h
+++ hashMap/hashMap.h
@@ -1,12 +1,24 @@
-/*
-	getHashCode
-	Hasing(modulas)
-	store, search, remove
-	create(compare)
-	put(hashCode Generator, compare)
-	remove(hashMap,data)
-	get(hashMap,key)
-	keys(hashMap)
-	dispose()
-	reHashing()
-*/
+#include "iterator.h"
+
+typedef int (*HashCodeGenerator)(void *key);
+
+typedef int (*compareFunc)(void *first,void *second);
+
+typedef struct {
+        void *buckets;
+        int capacity;
+        HashCodeGenerator getHashCode;
+        compareFunc compare;
+} HashMap;
+
+HashMap createHashMap(HashCodeGenerator getHashCode,compareFunc cmp,int capacity);
+
+int put(HashMap *map,void *key,void *data);
+
+void *getHashObject(HashMap *map,void *key);
+
+int removeHashObject(HashMap *map,void *key);
+
+Iterator keys(HashMap *map);
+
+void disposeHashMap(HashMap *map);
