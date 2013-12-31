c7b70042a18889bb9017214634ad7e4db9dee8d6
unknown <soumyagh@Insoumyagh.corporate.thoughtworks.com>
2013-12-31 13:40:48 +0530

    sorting of integer working

--- mergeSort/mergeSort.c
+++ mergeSort/mergeSort.c
@@ -2,7 +2,47 @@
 #include <memory.h>
 #include <stdlib.h>
 
+int merge(void* destination, void* left, void* right, size_t leftLength, 
+							size_t rightLength,size_t elementSize, comparator cmp ){
+        int i=0,j=0,k=0;
+        while(i < leftLength && j < rightLength){
+                if(cmp(left+(i*elementSize), right+(j*elementSize))){
+                        memmove(destination+(k*elementSize), left+(i*elementSize), elementSize);
+                        i++;
+                }
+                else{
+                        memmove(destination+(k*elementSize), right+(j*elementSize), elementSize);
+                        j++;
+                }
+                k++;
+        }
+        while( j < rightLength){
+                memmove(destination+(k*elementSize), right+(j*elementSize), elementSize);
+                j++;
+                k++;
+        }
+        while(i < leftLength){
+                memmove(destination+(k*elementSize), left+(i*elementSize), elementSize);
+                i++;
+                k++;
+        }
+        return 1;
+}
+
 void mergeSort(void* base, int numberOfElements, int elementSize, comparator comp){
-     if (numberOfElements <= 1)
-     	return ;
-     }
+        int mid = numberOfElements/2,i,j,leftLength,rightLength;
+        void* left = calloc(mid,elementSize);
+        void* right = calloc((numberOfElements-mid),elementSize);
+        if(numberOfElements < 2) return;
+        leftLength = mid;
+        rightLength = numberOfElements-mid;
+        for(i=0;i<mid;i++)
+                memmove(left+i*elementSize,base+i*elementSize,elementSize);
+        for(i=mid;i<numberOfElements;i++)
+                memmove(right+elementSize*(i-mid),base+elementSize*i,elementSize);
+        mergeSort(left,leftLength,elementSize,comp);
+        mergeSort(right,rightLength,elementSize,comp);
+        merge(base, left, right, leftLength, rightLength, elementSize, comp);
+        free(left);
+        free(right);
+}
--- mergeSort/mergeSortTest.c
+++ mergeSort/mergeSortTest.c
@@ -14,3 +14,20 @@ void test_single_element_is_always_sorted(){
         ASSERT(1==data[0]);
 }
 
+void test_sort_for_even_number_of_element_best_case(){
+        int count;
+        int data[] = {1,2,3,4};
+        mergeSort(&data,4,sizeof(int),compareInt);
+        for(count=0 ; count < 4 ; count++){
+                ASSERT(data[count]==count+1);
+        }
+}
+
+void test_sort_for_odd_number_of_element_worst_case(){
+        int count;
+        int data[] = {5,4,3,2,1};
+        mergeSort(&data,5,sizeof(int),compareInt);
+        for(count=0 ; count < 5 ; count++){
+                ASSERT(data[count]==count+1);
+        }
+}
